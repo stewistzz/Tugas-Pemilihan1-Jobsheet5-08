7b542fc6b4a41183935854a1d2e50c6aefef58ff
unknown <dimassetyonugroho30@gmail.com>
2023-10-10 08:08:16 +0700

    mengganti if else menjadi ternary

--- PemilihanPercobaan108.java
+++ PemilihanPercobaan108.java
@@ -8,10 +8,15 @@ public class PemilihanPercobaan108 {
         System.out.print("Masukkan angka = ");
         int angka = input08.nextInt();
 
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + "Bilangan Genap");
-        } else {
-            System.out.println("Angka " + angka + "BIlangan Ganjil");
-        }
+        // if (angka % 2 == 0) {
+        //     System.out.println("Angka " + angka + "Bilangan Genap");
+        // } else {
+        //     System.out.println("Angka " + angka + "BIlangan Ganjil");
+        // }
+
+        //Mengubah ke ternary operator
+        String result;
+        result = (angka %2 == 0) ? ("Angka " + angka + " bilangan genap") : ("Angka " + angka + " bilangan ganjil");
+        System.out.println(result);
     }
 }
