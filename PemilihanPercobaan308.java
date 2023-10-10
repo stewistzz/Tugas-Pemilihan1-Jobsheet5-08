becf903c2a3fb2274328be855895a61a918fb5e7
unknown <dimassetyonugroho30@gmail.com>
2023-10-10 16:13:49 +0700

    pemilihan percobaan3

---
+++ PemilihanPercobaan308.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan308 {
+    public static void main(String[] args) {
+        Scanner input08 = new Scanner(System.in);
+
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.print("Masukkan angka pertama = ");
+        angka1 = input08.nextDouble();
+        System.out.print("Masukkan angka kedua = ");
+        angka2 = input08.nextDouble();
+        System.out.print("Masukkan Operator (+ - * /) : ");
+        operator = input08.next().charAt(0);
+
+        // switch case
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println(angka1 + " + " + angka2 + " = " + hasil);
+                // break;
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println(angka1 + " - " + angka2 + " = " + hasil);
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println(angka1 + " * " + angka2 + " = " + hasil);
+                break;
+            case '/':
+                hasil = angka1 / angka2;
+                System.out.println(angka1 + " / " + angka2 + " = " + hasil);
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
